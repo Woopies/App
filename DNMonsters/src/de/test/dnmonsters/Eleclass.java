5c167b400de3fcbff554346731633fb3424a168f
Kevin Schroeder <kevin.schroeder@uni-duesseldorf.de>
2014-04-29 23:28:21 +0200

    remove phys and eleclass

--- DNMonsters/src/de/test/dnmonsters/Eleclass.java
+++
@@ -1,8 +0,0 @@
-package de.test.dnmonsters;
-
-public class Eleclass {
-	private int klasse;
-	public Eleclass(String s){
-	 
- }
-}
--- DNMonsters/src/de/test/dnmonsters/Physclass.java
+++
@@ -1,5 +0,0 @@
-package de.test.dnmonsters;
-
-public class Physclass {
-
-}
